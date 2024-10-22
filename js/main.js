a441719fd1da6fd1282024eb99b1d690743c456f
Volodymyr Udsianskiy <vv7827242@gmail.com>
2024-10-22 17:19:47 +0300

    add

---
+++ js/main.js
@@ -0,0 +1,24 @@
+let age = 15;
+console.log(age);
+
+const name = "Володимир";
+console.log(name);
+
+const isStudent = true;
+console.log(isStudent);
+
+const myString = "Краще шкодувати про те, що зробив, ніж про те, що не зробив.";
+console.log(myString);
+
+let myNumber = 32;
+const plus = myNumber + 10;
+console.log(plus);
+
+const myNull = null;
+console.log(myNull);
+
+
+const youName = prompt("Введи своє ім'я");
+alert("Привіт " + youName);
+
+
